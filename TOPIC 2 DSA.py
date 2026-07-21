04920ece9c177ab7188ecfae5b82f7ad68331da2
Enrie-dev <augustineenrie@gmail.com>
2026-07-21 14:59:08 +0800

    Add files via upload

---
+++ TOPIC 2 DSA.py
@@ -0,0 +1,18 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) //2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+numbers = [10, 20, 30, 40, 50, 60]
+print(binary_search(numbers, 50))
